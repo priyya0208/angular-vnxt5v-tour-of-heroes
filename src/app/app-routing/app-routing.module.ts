986ceefa573eb5c4a29e8745112cd285ca3df79a
priyya0208 <priyya.0208@gmail.com>
2019-09-18 19:44:42 +0530

    1 file modified, 1 added, 1 deleted

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { RouterModule, Routes } from '@angular/router'
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ RouterModule ]
+})
+export class AppRoutingModule { }
